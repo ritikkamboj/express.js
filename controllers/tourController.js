7a323132494581e3afe256d5f7b30b39578265fb
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-09-16 14:49:26 +0530

    implemented the aggregation pipeline for a real world task

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -237,3 +237,59 @@ exports.getTourStats = async (req, res) => {
   }
 
 }
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        },
+
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        }
+      },
+      {
+        $addFields: { month: '$_id' }
+      },
+      {
+        $project: {
+          _id: 0,
+
+        }
+      },
+      {
+        $sort: { numTourStarts: -1, month: 1 }
+      },
+      {
+        $limit: 12
+      }
+    ])
+
+    const resultsLength = plan.length
+    res.status(200).json({
+      status: 'success',
+      length: resultsLength,
+      data: { plan }
+    })
+  }
+  catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    })
+  }
+}
