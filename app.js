42fcfdbd0baaf5c9b14238c794623a188f41a24a
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-09-23 12:58:07 +0530

    applied global error handling middleware

--- app.js
+++ app.js
@@ -30,11 +30,29 @@ app.use('/api/v1/tours', tourRouter); // this is the middleware
 app.use('/api/v1/users', userRouter); // this is also middleware
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `There is no Data corresponding to ${req.originalUrl}`
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `There is no Data corresponding to ${req.originalUrl}`
+  // })
+  // next();
+  const err = new Error(`There is no Data corresponding to ${req.originalUrl}`);
+  err.statusCode = 404;
+  err.status = 'fail'
+  next(err);
+
+
+});
+
+// for Opeartional Error ( global error handling middleware )
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error'
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+
   })
-  next();
 })
 
 module.exports = app;
