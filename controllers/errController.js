4207ed92070468e9a3ff951cc6d5df66c625f46b
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-09-26 17:44:49 +0530

    we handle the CastError mongoose

--- controllers/errController.js
+++ controllers/errController.js
@@ -1,3 +1,9 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path} : ${err.value}.`
+    return new AppError(message, 400);
+}
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -10,7 +16,7 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
     // operational trusted error 
-    if (err.isOperation) {
+    if (err.isOperational) {
 
         res.status(err.statusCode).json({
             status: err.status,
@@ -19,6 +25,7 @@ const sendErrorProd = (err, res) => {
     }
     // programing and another type of error 
     else {
+
 
         console.log('ERROR', err);
         res.status(500).json({
@@ -34,15 +41,21 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error'
 
+
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res)
     }
     else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
-    }
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
+        let error = { ...err };
 
-    })
+
+        if (err.name === 'CastError') {
+            error = handleCastErrorDB(error)
+            console.log(error, 'Indian hockey');
+
+
+        }
+        sendErrorProd(error, res);
+    }
+
 }
