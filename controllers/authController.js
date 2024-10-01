57af9d86553e61ff38185ed52fefbbf2b856c525
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-01 12:14:24 +0530

    sign up of user by using jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,14 +2,27 @@
 
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    // below line makes any user declare itself as admin , to stop thart we have to change the line as :
+    // const newUser = await User.create(req.body);
+
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
     })
-});
+})
+
